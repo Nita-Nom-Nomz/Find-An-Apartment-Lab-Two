5709e1c9ae1556bfa1c4fb91fdda24ae419b48e8
Shenedia Ellington <artistic.nita@gmail.com>
2024-01-24 21:35:01 -0500

    added pseudo code via comment above code

--- FindApartment.py
+++ FindApartment.py
@@ -1,3 +1,30 @@
+"""pseudo code:
+
+have the code set for an apartment listing in two different ways.
+
+def apt_search1:
+use Named parameters
+must display a city (as a string), rent amount (as an init), number of beds (as an init), and pets are allowed (as a boolean).
+
+def apt_search2:
+use Named Parameters, Default Parameters, and possibly if/else might need to be added to this.
+must display a city, rent amount, number of beds, and pets are allowed.
+however the number of beds and pets is optional (allowed or not allowed).
+
+must be able to show in three different ways in print out:
+must used name arguments (specific names) for these specific tasks
+beds and pets not present on listing
+beds listed and pets not listed.
+pets listed and beds not listed.
+
+put in a lambda function (shorthand) for the last part of this assignment:
+one to add 100 to any given number that's entered in this setup (plus_one_hundred)
+one that squares (**) to any given number in this setup (square_num)
+one that concatenates (+) any given number in this setup (concatenate)
+one that divides (/) any number by 3 (divide_by_three)
+
+each of these functions must be called and printed out the console."""
+
 print("Apartment Search")
 def apt_search1(city, max_rent, min_beds, pets_allowed = True):
     if pets_allowed:
